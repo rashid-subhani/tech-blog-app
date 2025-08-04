9fe13e30302a2caa3ca2b9787a4befe0064380a7
rashidsubhani <ras_sub2003@yahoo.com>
2025-08-04 06:39:56 +0100

    Update DB connection for Railway deployment

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,16 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE,
-  process.env.MYSQLUSER,
-  process.env.MYSQLPASSWORD,
+  process.env.MYSQLDATABASE,   //  DB name
+  process.env.MYSQLUSER,       //  DB user
+  process.env.MYSQLPASSWORD,   // DB password
   {
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
+    host: process.env.MYSQLHOST, // DB host
+    port: process.env.MYSQLPORT || 3306, //  DB port
     dialect: "mysql",
   }
 );
 
 module.exports = sequelize;
+
 
